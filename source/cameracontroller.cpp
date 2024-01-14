da66f0ca23c1357ca92accf75c53b3ffa21280ea
Lvl3Mage <lvlthreemage@gmail.com>
2024-01-14 19:41:18 +0100

    Finished Camera controller, integrating into main.cpp

--- source/cameracontroller.cpp
+++ source/cameracontroller.cpp
@@ -1,12 +1,36 @@
+#include "cameracontroller.h"
+#include "world.h"
+#include "config.h"
+#include "utils.h"
+#include <memory>
+#include <cmath>
+#include <algorithm>
+using namespace std;
 CameraController::CameraController(u16* attachedMapMemory){
 
 }
-void CameraController::RenderCamera(){
-
+void CameraController::RenderCamera(unique_ptr<World> world){
+	for (int screenY = 0; screenY < camHeight; screenY++)
+	{
+		for (int screenX = 0; screenX < camWidth; screenX++)
+		{
+			int worldCoordX = screenX+camX;
+			int worldCoordY = screenY+camY;
+			int pos_mapMemory = (camHeight-1-screenY) * camWidth + screenX; //y coord inverted
+			mapMemory[pos_mapMemory] = world->GetWorldAt(worldCoordX,W_SIZE-1-worldCoordY); // world sampling inverted
+		}
+	}
+}
+void CameraController::ClampPosition(){
+	camX = clamp(camX, 0, W_SIZE - camWidth);
+	camY = clamp(camY, 0, W_SIZE - camHeight);
 }
 void CameraController::MoveCamera(int targetX, int targetY){
-
+	camX = targetX - camWidth/2;
+	camY = targetY - camHeight/2;
+	ClampPosition();
 }
 void CameraController::InterpolateCamera(int targetX, int targetY){
 
+	ClampPosition();
 }
--- source/cameracontroller.h
+++ source/cameracontroller.h
@@ -1,6 +1,10 @@
 #include <nds.h>
 #include <vector>
 #include "config.h"
+#include "world.h"
+#include <memory>
+
+using namespace std;
 
 class CameraController
 {
@@ -9,9 +13,10 @@ class CameraController
 	int camY=0;
 	const int camWidth = 32;
 	const int camHeight = 24;
+	void ClampPosition();
 public:
-	CameraController(u16* attachedMapMemory)
-	void RenderCamera();
+	CameraController(u16* attachedMapMemory);
+	void RenderCamera(unique_ptr<World> world);
 	void MoveCamera(int targetX, int targetY);
 	void InterpolateCamera(int targetX, int targetY);
 };
